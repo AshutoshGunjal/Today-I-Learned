dc28c5add9162fc3c90840f79fa475cd26fb008f
ashutoshgunjal <ashutosh.r.gunjal@gmail.com>
2023-12-09 12:46:10 -0500

    Dividing the interface into components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import "./style.css";
 
 function App() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-        <h1>Learning Wave!</h1>
-      </div>
+  const appTitle = "Learning Wave!";
 
-      <button className="btn btn-large btn-open">Share A Fact</button>
-    </header>
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>{appTitle}</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share A Fact</button>
+      </header>
+
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">New Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filters</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
